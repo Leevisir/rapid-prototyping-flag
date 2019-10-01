ad71d062b8d216e9436485dbef8e4d72503a9155
weijianli <leevi1044@outlook.com>
2019-10-01 01:39:03 -0500

    finish Show a grid of cards with the product titles

--- new-shopping-cart/src/App.js
+++ new-shopping-cart/src/App.js
@@ -1,8 +1,47 @@
 import React, { useEffect, useState } from 'react';
+import 'rbx/index.css';
+// import { Button, Container, Title, Tile, Box } from 'rbx';
+import ReactDOM from 'react-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     display: 'flex',
+//     flexWrap: 'wrap',
+//     justifyContent: 'space-around',
+//     overflow: 'hidden',
+//     backgroundColor: theme.palette.background.paper,
+//   },
+//   gridList: {
+//     width: 500,
+//     height: 450,
+//   },
+// }));
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
+  const classes = useStyles();
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
@@ -13,9 +52,21 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+
+    <React.Fragment>
+      <CssBaseline />
+        <Container maxWidth="sm">
+          <div className={classes.root}>
+            <Grid container spacing={3}>
+              {products.map(product =>
+                <Grid item xs={6} sm={3} key={product.sku}>
+                  <Paper className={classes.paper} key={product.sku}>{product.title}</Paper>
+                </Grid>
+              )}
+            </Grid>
+          </div>
+        </Container>
+    </React.Fragment>
   );
 };
 
